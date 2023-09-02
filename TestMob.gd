70b6c5622ba5047060c424c1c3476221584b7dfb
Camwog <cameronball.it@gmail.com>
2023-09-02 20:49:09 +1200

    Added mob attack anims

--- TestMob.gd
+++ TestMob.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+enum States {ONFLOOR, JUMP, RUN, LAND, ATTACK}
+var state
+var last_state
+var wall_counter = 0
 
 const speed = 75.0
 const JUMP_VELOCITY = -400.0
@@ -8,6 +12,12 @@ var heading = 1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func attack() :
+	state = States.ATTACK
+	velocity = Vector2(0,0)
+	$AnimatedSprite2D.play("attack")
+	await get_tree().create_timer(1).timeout
+	state = States.RUN 
 
 func _physics_process(delta):
 	
@@ -15,22 +25,29 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	velocity.x = speed * heading
+	if state != States.ATTACK :
+		velocity.x = speed * heading
 	
 	if is_on_wall() :
 		heading *= -1
 		
 		scale.x = -1
-		
+	
 		velocity.x = speed * heading
 		
-	if velocity.x > 0 and is_on_floor() :
+	if velocity.x != 0 and is_on_floor() :
 		$AnimatedSprite2D.play("run")
+		state = States.RUN
+		
+	if $RayCast2D.get_collider()  != null :
+		if $RayCast2D.get_collider().name == "Player":
+			attack()
+			print("hit")
 	
 	
 	
 	
 	move_and_slide()
 	
-	
+	last_state = state
 	pass
